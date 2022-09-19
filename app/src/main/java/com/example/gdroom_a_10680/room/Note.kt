c6d3334496636a80162ec82c31eb31f206efe203
tebri-artanto <71914107+tebri-artanto@users.noreply.github.com>
2022-09-19 13:21:48 +0700

    commit

--- app/src/main/java/com/example/gdroom_a_10680/room/Note.kt
+++ app/src/main/java/com/example/gdroom_a_10680/room/Note.kt
@@ -1,7 +1,9 @@
 package com.example.gdroom_a_10680.room
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity
 data class Note(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
